fc6056241f46a129c273dcb2997954da5045584c
Василий Романов <myhappy1988@gmail.com>
2022-02-17 12:59:37 +0300

    Update mobile version css

---
+++ js/questions/q2.js
@@ -0,0 +1,21 @@
+const q2 = () => {
+    const markupQ2 = 
+    `<div class="question-2">
+        <h3>Есть ли у Вас дети?</h3>
+        <div class="questions-group">
+            <label for="">
+                <input type="checkbox" name="" id="">
+                <span>Да, один ребенок</span>
+            </label>
+            <label for="">
+                <input type="checkbox" name="" id="">
+                <span>Да, два и более</span>
+            </label>
+            <label for="">
+                <input type="checkbox" name="" id="">
+                <span>Нет детей</span>
+            </label>
+        </div>
+    </div>`;
+}
+export default q2;
