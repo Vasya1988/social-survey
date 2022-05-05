b7465fd385e42b02ebcb7e738ee5fd3f00429caf
Василий Романов <myhappy1988@gmail.com>
2022-05-05 16:44:34 +0300

    add answersExport

--- js/finishedPage.js
+++ js/finishedPage.js
@@ -16,11 +16,28 @@ const finishedPage = () => {
 
     const pageNumber = 9;
 
+    
+
     const renderPage = () => {
         document.getElementById('app').insertAdjacentHTML('afterbegin', markupFinishedPage);
 
         console.log(main.state.person.personCard);
         main.progressBar(pageNumber);
+
+        const answersExport = {
+
+            'Где вы предпочитаете покупать детскую обувь?': main.state.person.personCard.whichBrand.answer,
+
+            'Сталкиваетесь ли вы при выборе детской обуви со следующими проблемами?': main.state.person.personCard.shoesTrouble.answer,
+
+            'Есть ли у Вас дети?': main.state.person.personCard.children.answer,
+
+            'Укажите, пожалуйста, ваше имя': main.state.person.personCard.personName,
+
+            'extra': main.state.person.personCard.personAnswers
+
+        };
+        console.log(answersExport)
     }
 
     return renderPage();
